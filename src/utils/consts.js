7b5d285ff23e682cf0bcdc963a6e087e53fc8107
JesusChiro <jesus_chiroque@hotmail.com>
2025-04-24 15:20:41 -0500

    add Hero component with slider functionality and SLIDERHEADER data

---
+++ src/utils/consts.js
@@ -0,0 +1,14 @@
+export const SLIDERHEADER = [
+    {
+        AltSlider: 'barber - Agenda Smart',
+        Img: './hero/barber.jpg'
+    },
+    {
+        AltSlider: 'Medico - Agenda Smart',
+        Img: './hero/doctor.png',
+    },
+    {
+        AltSlider: 'Enfermera - Agenda Smart',
+        Img: './hero/nurse.png',
+    },
+]
