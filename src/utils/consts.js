8a41a547ffebbd3b07a0dd83a4a5634e09b94258
JesusChiro <jesus_chiroque@hotmail.com>
2025-04-24 22:40:36 -0500

    Add new SVG icons and update Info component with automated appointment features
    - Added SVG icons for devices, notifications, plans, and user experience.
    - Updated the Info component to display a list of features with corresponding icons.
    - Introduced a new constant `BOXES` in `consts.js` to manage feature titles, descriptions, and icons.
    - Changed font fallback in global CSS from Poppins to Raleway for a consistent typography.

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -12,3 +12,36 @@ export const SLIDER = [
         Img: './hero/nurse.png',
     },
 ]
+
+export const BOXES = [
+    {
+        title: 'Citas automatizadas',
+        description: 'Olvídate del papeleo y la gestión manual.',
+        icon: './info/calendar.svg',
+    },
+    {
+        title: 'Recordatorios automáticos',
+        description: 'Reduce ausencias con notificaciones por WhatsApp y correo.',
+        icon: './info/notification.svg',
+    },
+    {
+        title: 'Acceso desde cualquier dispositivo ',
+        description: 'Disponible en web y app móvil (Android).',
+        icon: './info/devices.svg',
+    },
+    {
+        title: 'Agenda personalizada',
+        description: 'Configura horarios, duración de citas y disponibilidad.',
+        icon: './info/agend.svg',
+    },
+    {
+        title: 'Interfaz fácil de usar',
+        description: 'Diseñada para todos, sin importar su experiencia en tecnología.',
+        icon: './info/ux.svg',
+    },
+    {
+        title: 'Planes accesibles',
+        description: 'Para negocios de cualquier tamaño.',
+        icon: './info/plans.svg',
+    },
+]
