e918dde9ca2a9c7e14ab802cf85c05c1caafe717
JesusChiro <jesus_chiroque@hotmail.com>
2025-05-01 22:19:04 -0500

    Refactor SLIDER data structure: update content and order for improved clarity and consistency

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,18 +1,41 @@
 export const SLIDER = [
     {
-        AltSlider: 'barber - Agenda Smart',
-        Img: './hero/barber.jpg'
+        AltSlider: "Medico - Agenda Smart",
+        Img: "/hero/doctor.png",
+        ImgMovil: "/hero/doctor_movil.png",
+        breadcrumb1: "Clínicas",
+        breadcrumb2: "Consultorios",
+        breadcrumb3: "Hospitales",
+        title1: "Gestiona",
+        title2: "tus citas médicas",
+        title3: "sin esfuerzo.",
+        description:
+            "Automatiza tu agenda y envía recordatorios para reducir cancelaciones.",
     },
     {
-        AltSlider: 'Medico - Agenda Smart',
-        Img: './hero/doctor.png',
+        AltSlider: "Enfermera - Agenda Smart",
+        Img: "/hero/nurse.png",
+        ImgMovil: "/hero/nurse_movil.png",
+        breadcrumb1: "SPA",
+        breadcrumb2: "Centro de Belleza",
+        breadcrumb3: "Podología",
+        title1: "Relájate y deja que",
+        title2: 'tu agenda trabaje por ti.',
+        description:
+            "Permite que tus clientes reserven fácil y rápido, sin confusiones.",
     },
     {
-        AltSlider: 'Enfermera - Agenda Smart',
-        Img: './hero/nurse.png',
+        AltSlider: "barber - Agenda Smart",
+        Img: "/hero/barber.jpg",
+        ImgMovil: "/hero/barber_movil.png",
+        breadcrumb1: "Peluquería",
+        breadcrumb2: "Barbería",
+        title1: "Más citas, más clientes,",
+        title2: 'más ingresos.',
+        description:
+            "Olvídate del caos y agenda cada servicio de manera rápida y eficiente.",
     },
-]
-
+];
 export const BOXES = [
     {
         title: 'Citas automatizadas',
